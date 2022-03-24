da27386e68c72de8d82b676c3034829660c41fef
Mintesnot Yismaw <yismawmintesnot@gmail.com>
2022-03-24 02:05:19 -0700

    fixed rc-slider issue

--- components/products-filter/index.js
+++ components/products-filter/index.js
@@ -1,21 +1,24 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import Checkbox from './form-builder/checkbox';
-import CheckboxColor from './form-builder/checkbox-color';
-import Slider from 'rc-slider';
-import Tooltip from 'rc-tooltip';
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Checkbox from "./form-builder/checkbox";
+import CheckboxColor from "./form-builder/checkbox-color";
+import Slider /* , { Range } */ from "rc-slider";
+import Tooltip from "rc-tooltip";
 import { useForm } from "react-hook-form";
 
 // data
-import productsTypes from './../../utils/data/products-types';
-import productsColors from './../../utils/data/products-colors';
-import productsSizes from './../../utils/data/products-sizes';
+import productsTypes from "./../../utils/data/products-types";
+import productsColors from "./../../utils/data/products-colors";
+import productsSizes from "./../../utils/data/products-sizes";
 
-const { createSliderWithTooltip } = Slider;
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
+
+//const { createSliderWithTooltip } = Slider;
+//const Range = createSliderWithTooltip(Slider.Range);
 const { Handle } = Slider;
 
-const handle = props => {
+const handle = (props) => {
   const { value, dragging, index, ...restProps } = props;
   return (
     <Tooltip
@@ -35,27 +38,32 @@ const ProductsFilter = () => {
   const [filtersOpen, setFiltersOpen] = useState(false);
 
   const addQueryParams = () => {
+    console.log('addQueryParams')
     // query params changes
-  }
+  };
 
   return (
     <form className="products-filter" onChange={addQueryParams}>
-      <button type="button" 
-        onClick={() => setFiltersOpen(!filtersOpen)} 
-        className={`products-filter__menu-btn ${filtersOpen ? 'products-filter__menu-btn--active' : ''}`}>
-          Add Filter <i className="icon-down-open"></i>
+      <button
+        type="button"
+        onClick={() => setFiltersOpen(!filtersOpen)}
+        className={`products-filter__menu-btn ${
+          filtersOpen ? "products-filter__menu-btn--active" : ""
+        }`}
+      >
+        Add Filter <i className="icon-down-open"></i>
       </button>
-      
-      <div className={`products-filter__wrapper ${filtersOpen ? 'products-filter__wrapper--open' : ''}`}>
+
+      <div
+        className={`products-filter__wrapper ${
+          filtersOpen ? "products-filter__wrapper--open" : ""
+        }`}
+      >
         <div className="products-filter__block">
           <button type="button">Product type</button>
           <div className="products-filter__block__content">
-            {productsTypes.map(type => (
-              <Checkbox 
-                key={type.id} 
-                name="product-type" 
-                label={type.name} 
-              />
+            {productsTypes.map((type) => (
+              <Checkbox key={type.id} name="product-type" label={type.name} />
             ))}
           </div>
         </div>
@@ -63,39 +71,53 @@ const ProductsFilter = () => {
         <div className="products-filter__block">
           <button type="button">Price</button>
           <div className="products-filter__block__content">
-            <Range min={0} max={20} defaultValue={[3, 10]} tipFormatter={value => `${value}%`} />
+            <Range
+              min={0}
+              max={20}
+              defaultValue={[3, 10]}
+              tipFormatter={(value) => `${value}%`}
+            />
           </div>
         </div>
-        
+
         <div className="products-filter__block">
           <button type="button">Size</button>
           <div className="products-filter__block__content checkbox-square-wrapper">
-            {productsSizes.map(type => (
-              <Checkbox 
-                type="square" 
-                key={type.id} 
-                name="product-size" 
-                label={type.label} />
+            {productsSizes.map((type) => (
+              <Checkbox
+                type="square"
+                key={type.id}
+                name="product-size"
+                label={type.label}
+              />
             ))}
           </div>
         </div>
-        
+
         <div className="products-filter__block">
           <button type="button">Color</button>
           <div className="products-filter__block__content">
             <div className="checkbox-color-wrapper">
-              {productsColors.map(type => (
-                <CheckboxColor key={type.id} name="product-color" color={type.color} />
+              {productsColors.map((type) => (
+                <CheckboxColor
+                  key={type.id}
+                  name="product-color"
+                  color={type.color}
+                />
               ))}
             </div>
           </div>
         </div>
 
-        <button type="submit" className="btn btn-submit btn--rounded btn--yellow">Apply</button>
+        <button
+          type="submit"
+          className="btn btn-submit btn--rounded btn--yellow"
+        >
+          Apply
+        </button>
       </div>
     </form>
-  )
-}
-  
-export default ProductsFilter
-  
+  );
+};
+
+export default ProductsFilter;
