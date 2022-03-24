e6c00fb3a828596c6da92d1454561d2edf064829
Mintesnot Yismaw <yismawmintesnot@gmail.com>
2022-03-24 02:02:22 -0700

    edited bread crumb

--- components/breadcrumb/index.js
+++ components/breadcrumb/index.js
@@ -1,13 +1,16 @@
-const Breadcrumb = () => (
+const Breadcrumb = ({ currentPage }) => (
   <section className="breadcrumb">
     <div className="container">
       <ul className="breadcrumb-list">
-        <li><a href="#"><i className="icon-home"></i></a></li>
-        <li>All Products</li>
+        <li>
+          <a href="#">
+            <i className="icon-home"></i>
+          </a>
+        </li>
+        <li>{currentPage ? currentPage : "All Products"}</li>
       </ul>
     </div>
   </section>
 );
 
-
-export default Breadcrumb
+export default Breadcrumb;
