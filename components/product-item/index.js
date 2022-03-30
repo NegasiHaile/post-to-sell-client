235769d81724cec760e6767407baf883f1f95b43
mintesnot123 <yismawmintesnot@gmai.com>
2022-03-30 05:15:00 -0700

    edited product item

--- components/product-item/index.js
+++ components/product-item/index.js
@@ -1,50 +1,67 @@
-import Link from 'next/link';
-import { some } from 'lodash';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleFavProduct } from './../../store/actions/userActions';
+import Link from "next/link";
+import { some } from "lodash";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleFavProduct } from "./../../store/actions/userActions";
 
-const ProductItem = ({ discount, productImage, id, name, price, currentPrice }) => {
+const ProductItem = ({
+  discount,
+  productImage,
+  tag,
+  id,
+  name,
+  price,
+  currentPrice,
+}) => {
   const dispatch = useDispatch();
-  const { favProducts } = useSelector(state => state.user);
+  const { favProducts } = useSelector((state) => state.user);
 
-  const isFavourite = some(favProducts, productId => productId === id);
+  const isFavourite = some(favProducts, (productId) => productId === id);
 
   const toggleFav = () => {
-    dispatch(toggleFavProduct(
-      { 
+    dispatch(
+      toggleFavProduct({
         id,
-      }
-    ))
-  }
+      })
+    );
+  };
 
   return (
     <div className="product-item">
       <div className="product__image">
-        <button type="button" onClick={toggleFav} className={`btn-heart ${isFavourite ? 'btn-heart--active' : ''}`}><i className="icon-heart"></i></button>
+        <button
+          type="button"
+          onClick={toggleFav}
+          className={`btn-heart ${isFavourite ? "btn-heart--active" : ""}`}
+        >
+          <i className="icon-heart"></i>
+        </button>
 
         <Link href={`/product/${id}`}>
           <a>
             <img src={productImage} alt="product" />
-            {discount && 
-              <span className="product__discount">{discount}%</span>
-            }
+            {tag && tag !== "" && (
+              <span className="product__discount">{tag}</span>
+            )}
           </a>
         </Link>
       </div>
-      
+
       <div className="product__description">
         <h3>{name}</h3>
-        <div className={"product__price " + (discount ? 'product__price--discount' : '')} >
-          <h4>${ currentPrice }</h4>
-
-          {discount &&  
-            <span>${ price }</span>
+        <div
+          className={
+            "product__price " + (price ? "product__price--discount" : "")
           }
+        >
+          <h4>${currentPrice}</h4>
+
+          {price && (
+            <span style={{ textDecoration: "line-through" }}>${price}</span>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 };
 
-
-export default ProductItem
+export default ProductItem;
