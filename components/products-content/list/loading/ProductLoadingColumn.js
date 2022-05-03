9e0df215b1f889935aa96862333b19461c32bd87
BlueCloud15696 <webmaster15696@gmail.com>
2022-05-03 10:02:30 +0300

    added: featured and advert loading skeleton

---
+++ components/products-content/list/loading/ProductLoadingColumn.js
@@ -0,0 +1,12 @@
+import ProductItemLoading from "./../../../product-item/loading";
+
+const ProductsLoading = () => {
+  return (
+    <section className="products-list-column">
+      <ProductItemLoading />
+      <ProductItemLoading />
+    </section>
+  );
+};
+
+export default ProductsLoading;
